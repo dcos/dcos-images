95eed5af47132510a0c0b6c1080f1af5d19f022c
Charles Provencher <cprovencher@mesosphere.com>
2018-06-27 14:20:30 -0700

    Jenkinsfile

---
+++ get_packer.sh
@@ -0,0 +1,6 @@
+apt-get install -y curl
+curl -L -O https://releases.hashicorp.com/packer/1.2.4/packer_1.2.4_linux_amd64.zip
+unzip ./packer*.zip
+chmod +x packer
+mv packer /usr/local/bin
+packer --help
