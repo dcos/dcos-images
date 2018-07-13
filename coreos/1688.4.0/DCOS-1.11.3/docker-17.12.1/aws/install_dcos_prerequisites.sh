de2e5ef749b8f7114b304c6fa16eaf39cad209ce
Gauri Powale <gpowale@mesosphere.com>
2018-07-13 15:40:21 -0700

    Un-triggering Oracle & Triggering CoreOS.

--- coreos/1688.4.0/DCOS-1.11.3/docker-17.12.1/aws/install_dcos_prerequisites.sh
+++ coreos/1688.4.0/DCOS-1.11.3/docker-17.12.1/aws/install_dcos_prerequisites.sh
@@ -3,3 +3,5 @@
 sudo systemctl disable locksmithd
 sudo systemctl stop locksmithd
 sudo systemctl restart docker # Restarting docker to ensure its ready. Seems like its not during first usage.
+
+echo "test-coreos"
