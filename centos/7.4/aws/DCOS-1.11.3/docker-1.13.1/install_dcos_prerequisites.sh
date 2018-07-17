5f703b728b8dad54f62cf880de59fe787efb1f0d
Senthil Kumaran <skumaran@mesosphere.io>
2018-07-17 16:41:47 -0700

    updated Cent OS 7.4 AMI

--- centos/7.4/aws/DCOS-1.11.3/docker-1.13.1/install_dcos_prerequisites.sh
+++ centos/7.4/aws/DCOS-1.11.3/docker-1.13.1/install_dcos_prerequisites.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 sudo setenforce 0 && \
 sudo sed -i --follow-symlinks 's/^SELINUX=.*/SELINUX=disabled/g' /etc/sysconfig/selinux
+
+# DC/OS pre-reqs from dcos/dcos cloud images.
+
+# Install base packages
+
+sudo yum --nogpgcheck -t -y groupinstall core
+sudo yum --nogpgcheck -t -y install openssh-server grub2 tuned kernel chrony
+sudo yum --nogpgcheck -t -y install cloud-init cloud-utils-growpart
+sudo yum  -C -t -y remove NetworkManager firewalld --setopt="clean_requirements_on_remove=1"
+
+# Install docker 
 sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
 [dockerrepo]
 name=Docker Repository
@@ -9,7 +20,7 @@ enabled=1
 gpgcheck=1
 gpgkey=https://yum.dockerproject.org/gpg
 EOF
-# sudo yum -y update --exclude="docker-engine*"
+
 sudo mkdir -p /etc/systemd/system/docker.service.d
 sudo tee /etc/systemd/system/docker.service.d/override.conf <<- EOF
 [Service]
@@ -19,6 +30,9 @@ EOF
 sudo yum install -y docker-engine-1.13.1
 sudo systemctl start docker
 sudo systemctl enable docker
+
+# Install other pre-requisites
+
 sudo yum install -y wget
 sudo yum install -y git
 sudo yum install -y unzip
@@ -26,10 +40,11 @@ sudo yum install -y curl
 sudo yum install -y xz
 sudo yum install -y ipset
 sudo yum install -y ntp
+
 sudo systemctl enable ntpd
 sudo systemctl start ntpd
+
 sudo getent group nogroup || sudo groupadd nogroup
 sudo getent group docker || sudo groupadd docker
-sudo touch /opt/dcos-prereqs.installed
 
-echo "test-centos"
+sudo touch /opt/dcos-prereqs.installed
