c104c44fd62d00f579b89d8968fdea8665450551
Charles Provencher <cprovencher@mesosphere.com>
2018-07-31 14:15:28 -0700

    Add missing desired_cluster_profile.tfvars for CoreOS

--- coreos/1688.4.0/aws/DCOS-1.11.3/docker-17.12.1/install_dcos_prerequisites.sh
+++ coreos/1688.4.0/aws/DCOS-1.11.3/docker-17.12.1/install_dcos_prerequisites.sh
@@ -5,13 +5,13 @@ sudo systemctl stop locksmithd
 sudo systemctl restart docker # Restarting docker to ensure its ready. Seems like its not during first usage.
 
 # This works around https://github.com/coreos/bugs/issues/426
-mkdir -p /etc/systemd/system/sshd.service.d
-touch /etc/systemd/system/sshd.service.d/override.conf
-echo '[Service]' >> /etc/systemd/system/sshd.service.d/override.conf
-echo 'Restart=always' >> /etc/systemd/system/sshd.service.d/override.conf
+sudo mkdir -p /etc/systemd/system/sshd.service.d
+sudo touch /etc/systemd/system/sshd.service.d/override.conf
+sudo echo '[Service]' >> /etc/systemd/system/sshd.service.d/override.conf
+sudo echo 'Restart=always' >> /etc/systemd/system/sshd.service.d/override.conf
 # Without this we get:
 # sshd.service: Start request repeated too quickly.
 # Failed to start OpenSSH server daemon.
 # sshd.service: Unit entered failed state.
 # sshd.service: Failed with result 'start-limit-hit'.
-echo 'RestartSec=3s' >> /etc/systemd/system/sshd.service.d/override.conf
+sudo echo 'RestartSec=3s' >> /etc/systemd/system/sshd.service.d/override.conf
