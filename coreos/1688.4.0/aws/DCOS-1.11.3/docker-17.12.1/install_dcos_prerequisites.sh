7c04a39de76a32850f888aed6b50b2127d7a2916
Charles Provencher <cprovencher@mesosphere.com>
2018-07-31 13:35:44 -0700

    Update coreos/1688.4.0/aws/DCOS-1.11.3/docker-17.12.1/install_dcos_prerequisites.sh

--- coreos/1688.4.0/aws/DCOS-1.11.3/docker-17.12.1/install_dcos_prerequisites.sh
+++ coreos/1688.4.0/aws/DCOS-1.11.3/docker-17.12.1/install_dcos_prerequisites.sh
@@ -3,3 +3,15 @@
 sudo systemctl disable locksmithd
 sudo systemctl stop locksmithd
 sudo systemctl restart docker # Restarting docker to ensure its ready. Seems like its not during first usage.
+
+# This works around https://github.com/coreos/bugs/issues/426
+mkdir -p /etc/systemd/system/sshd.service.d
+touch /etc/systemd/system/sshd.service.d/override.conf
+echo '[Service]' >> /etc/systemd/system/sshd.service.d/override.conf
+echo 'Restart=always' >> /etc/systemd/system/sshd.service.d/override.conf
+# Without this we get:
+# sshd.service: Start request repeated too quickly.
+# Failed to start OpenSSH server daemon.
+# sshd.service: Unit entered failed state.
+# sshd.service: Failed with result 'start-limit-hit'.
+echo 'RestartSec=3s' >> /etc/systemd/system/sshd.service.d/override.conf
