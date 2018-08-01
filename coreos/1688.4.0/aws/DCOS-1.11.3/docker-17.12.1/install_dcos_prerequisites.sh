067d03e086441105082da0407526feaa4da5d31c
Charles Provencher <cprovencher@mesosphere.com>
2018-07-31 18:12:26 -0700

    Fix permissions error with install_prereqs

--- coreos/1688.4.0/aws/DCOS-1.11.3/docker-17.12.1/install_dcos_prerequisites.sh
+++ coreos/1688.4.0/aws/DCOS-1.11.3/docker-17.12.1/install_dcos_prerequisites.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-sudo systemctl disable locksmithd
-sudo systemctl stop locksmithd
-sudo systemctl restart docker # Restarting docker to ensure its ready. Seems like its not during first usage.
+systemctl disable locksmithd
+systemctl stop locksmithd
+systemctl restart docker # Restarting docker to ensure its ready. Seems like its not during first usage.
 
 # This works around https://github.com/coreos/bugs/issues/426
-sudo mkdir -p /etc/systemd/system/sshd.service.d
-sudo touch /etc/systemd/system/sshd.service.d/override.conf
-sudo echo '[Service]' >> /etc/systemd/system/sshd.service.d/override.conf
-sudo echo 'Restart=always' >> /etc/systemd/system/sshd.service.d/override.conf
+mkdir -p /etc/systemd/system/sshd.service.d
+touch /etc/systemd/system/sshd.service.d/override.conf
+echo '[Service]' >> /etc/systemd/system/sshd.service.d/override.conf
+echo 'Restart=always' >> /etc/systemd/system/sshd.service.d/override.conf
 # Without this we get:
 # sshd.service: Start request repeated too quickly.
 # Failed to start OpenSSH server daemon.
 # sshd.service: Unit entered failed state.
 # sshd.service: Failed with result 'start-limit-hit'.
-sudo echo 'RestartSec=3s' >> /etc/systemd/system/sshd.service.d/override.conf
+echo 'RestartSec=3s' >> /etc/systemd/system/sshd.service.d/override.conf
