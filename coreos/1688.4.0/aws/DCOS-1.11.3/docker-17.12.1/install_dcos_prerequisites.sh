8ad2a312aa808186efb87c2a29ca236004b47f6f
Charles Provencher <cprovencher@mesosphere.com>
2018-08-01 18:31:26 -0700

    Modify rc.local in install_prereqs for coreos/1688.4.0/aws/DCOS-1.11.3/docker-17.12.1

--- coreos/1688.4.0/aws/DCOS-1.11.3/docker-17.12.1/install_dcos_prerequisites.sh
+++ coreos/1688.4.0/aws/DCOS-1.11.3/docker-17.12.1/install_dcos_prerequisites.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-systemctl disable locksmithd
-systemctl stop locksmithd
-systemctl restart docker # Restarting docker to ensure its ready. Seems like its not during first usage.
-
 # This works around https://github.com/coreos/bugs/issues/426
 mkdir -p /etc/systemd/system/sshd.service.d
 touch /etc/systemd/system/sshd.service.d/override.conf
@@ -15,3 +11,27 @@ echo 'Restart=always' >> /etc/systemd/system/sshd.service.d/override.conf
 # sshd.service: Unit entered failed state.
 # sshd.service: Failed with result 'start-limit-hit'.
 echo 'RestartSec=3s' >> /etc/systemd/system/sshd.service.d/override.conf
+
+
+cat<<EOF > /etc/rc.local
+#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will "exit 0" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
+systemctl disable locksmithd
+systemctl stop locksmithd
+systemctl mask locksmithd
+systemctl disable update-engine # Disabling automatic updates.
+systemctl stop update-engine
+systemctl mask update-engine
+systemctl restart docker # Restarting docker to ensure its ready. Seems like its not during first usage
+exit 0
+EOF
