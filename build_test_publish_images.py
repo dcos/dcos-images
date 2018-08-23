292b35f033d802b48e961c4e932f5465d695cec5
Gauri Powale <gpowale@mesosphere.com>
2018-08-23 00:23:22 -0700

    Testing 1800.7.0 against DC/OS 1.11.4

--- build_test_publish_images.py
+++ build_test_publish_images.py
@@ -213,7 +213,10 @@ def run_integration_tests(ssh_user, master_public_ips, master_private_ips, priva
     user_and_host = ssh_user + '@' + master_public_ips[0]
 
     # Running integration tests
-    subprocess.run(["ssh", "-o", "StrictHostKeyChecking=no", user_and_host, pytest_cmd], check=False, cwd=tf_dir)
+    try:
+         subprocess.run(["ssh", "-o", "StrictHostKeyChecking=no", user_and_host, pytest_cmd], check=True, cwd=tf_dir)
+     except Exception as e:
+         print(repr(e))
 
 
 def run_framework_tests(master_public_ip, tf_dir, s3_bucket='osqual-frameworks-artifacts'):
