528a2fb105bced147320f853ebe61e8dfa10e8f7
Gauri Powale <gpowale@mesosphere.com>
2018-11-13 10:41:16 -0800

    Triggering tests.

--- coreos/1855.5.0/aws/DCOS-1.12.0/docker-18.06.1/install_dcos_prerequisites.sh
+++ coreos/1855.5.0/aws/DCOS-1.12.0/docker-18.06.1/install_dcos_prerequisites.sh
@@ -2,4 +2,4 @@
 
 sudo systemctl disable locksmithd
 sudo systemctl stop locksmithd
-sudo systemctl restart docker # Restarting docker to ensure its ready. Seems like its not during first usage.
+sudo systemctl restart docker # Restarting docker to ensure its ready. Seems like it's not during first usage.
