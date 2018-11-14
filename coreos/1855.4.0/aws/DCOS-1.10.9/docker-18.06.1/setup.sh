399cf27a6d74017bb91091470593d8e017fd53de
Gauri Powale <gpowale@mesosphere.com>
2018-11-14 11:42:37 -0800

    Triggering tests.

--- coreos/1855.4.0/aws/DCOS-1.10.9/docker-18.06.1/setup.sh
+++ coreos/1855.4.0/aws/DCOS-1.10.9/docker-18.06.1/setup.sh
@@ -6,4 +6,4 @@ sudo systemctl mask locksmithd
 sudo systemctl disable update-engine # Disabling automatic updates.
 sudo systemctl stop update-engine
 sudo systemctl mask update-engine
-sudo systemctl restart docker # Restarting docker to ensure its ready. Seems like its not during first usage
+sudo systemctl restart docker # Restarting docker to ensure its ready. Seems like it's not during first usage
