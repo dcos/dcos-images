208c9100ef58a84cb8bc22c31f0d3d37b5da3bd7
VerginiyaStoyanova <vstoyanova.c@mesosphere.io>
2019-01-16 09:01:24 +0200

    Fixing PR

--- coreos/1855.5.0/aws/DCOS-1.10.10/docker-18.06.1/install_dcos_prerequisites.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-sudo systemctl disable locksmithd
-sudo systemctl stop locksmithd
-sudo systemctl restart docker # Restarting docker to ensure its ready. Seems like it's not during first usage.
