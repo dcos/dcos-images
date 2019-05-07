5120b3aa3bdeb27ce3b0613e6a2240cf5352d521
VerginiyaStoyanova <vstoyanova.c@mesosphere.io>
2019-05-07 14:55:53 +0300

    Qualifying DC/OS 1.13.0 against CoreOS 2023.4.0

--- coreos/2023.4.0/aws/DCOS-1.12.3/docker-18.06.1/install_dcos_prerequisites.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-sudo systemctl disable locksmithd
-sudo systemctl stop locksmithd
-sudo systemctl restart docker # Restarting docker to ensure its ready. Seems like its not during first usage.
