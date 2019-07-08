53c35b93bf4c129db9a1867cd7983385b43b23f6
Arsen Hayrapetyan <ahayrapetyan@mesosphere.com>
2019-07-08 16:28:21 +0200

    Fix terraform config

--- coreos/2135.4.0/aws/DCOS-1.13.2/docker-18.06.3/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-sudo systemctl disable locksmithd
-sudo systemctl stop locksmithd
-sudo systemctl mask locksmithd
-
-sudo systemctl disable update-engine # Disabling automatic updates.
-sudo systemctl stop update-engine
-sudo systemctl mask update-engine
-sudo systemctl restart docker # Restarting docker to ensure its ready. Seems like its not during first usage
