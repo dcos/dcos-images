667f851a9229460778014e5e5a053f2a74b554a9
Arsen Hayrapetyan <ahairape@gmail.com>
2019-08-28 14:21:57 +0200

    Adjust docker installation to RHEL8.0

--- rhel/8.0/aws/DCOS-1.13.3/docker-18.09.2-ce/selinux_disabled/install_dcos_prerequisites.sh
+++ rhel/8.0/aws/DCOS-1.13.3/docker-18.09.2-ce/selinux_disabled/install_dcos_prerequisites.sh
@@ -8,10 +8,7 @@ sudo bash -c 'echo -e "nameserver 8.8.8.8\n" >> /etc/resolv.conf'
 sudo yum install -y yum-utils
 
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum makecache fast
-
-sudo yum install -y http://mirror.centos.org/centos/7/extras/x86_64/Packages/container-selinux-2.74-1.el7.noarch.rpm
-sudo yum install -y http://mirror.centos.org/centos/7/extras/x86_64/Packages/pigz-2.3.3-1.el7.centos.x86_64.rpm
+sudo yum makecache
 
 sudo yum install -y docker-ce-18.09.2 docker-ce-cli-18.09.2 containerd.io
 sudo systemctl enable docker
