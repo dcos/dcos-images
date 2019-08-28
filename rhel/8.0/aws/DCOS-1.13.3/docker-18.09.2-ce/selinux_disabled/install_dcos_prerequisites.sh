3a912cce6458d05c6febc3d3473c19b15dc70d9b
Arsen Hayrapetyan <ahairape@gmail.com>
2019-08-28 16:30:18 +0200

    Install Docker with

--- rhel/8.0/aws/DCOS-1.13.3/docker-18.09.2-ce/selinux_disabled/install_dcos_prerequisites.sh
+++ rhel/8.0/aws/DCOS-1.13.3/docker-18.09.2-ce/selinux_disabled/install_dcos_prerequisites.sh
@@ -10,7 +10,7 @@ sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum makecache
 
-sudo yum install -y docker-ce docker-ce-cli containerd.io
+sudo yum install -y --nobest docker-ce docker-ce-cli containerd.io
 sudo systemctl enable docker
 sudo systemctl start docker
 
