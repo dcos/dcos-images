5d9b9a23bdfba7cff2ec1fcb3b54ca57ba14f313
Arsen Hayrapetyan <ahairape@gmail.com>
2019-09-06 14:05:00 +0200

    Debug

--- build_test_publish_images.py
+++ build_test_publish_images.py
@@ -320,6 +320,7 @@ def setup_cluster_and_test(build_dir, tf_dir, dry_run, tests, publish_step, run_
 
     try:
         print('Directory where we are runnint terraform apply:' + str(tf_dir))
+        print('License file: ' + os.system('ls -la /tmp/licnse.txt'))
         subprocess.run(tf_apply_cmd.split(), check=True, cwd=tf_dir)
 
         if publish_step == PUBLISH_STEP_DCOS_INSTALLATION:
