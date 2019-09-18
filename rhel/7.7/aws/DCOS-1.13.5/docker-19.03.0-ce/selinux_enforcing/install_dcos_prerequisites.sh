29431de25c06e28813d543f2070f73c345f323c3
VerginiyaStoyanova <vstoyanova@mesosphere.com>
2019-09-18 15:06:59 +0300

    Qualify in enforced SELinux mode

---
+++ rhel/7.7/aws/DCOS-1.13.5/docker-19.03.0-ce/selinux_enforcing/install_dcos_prerequisites.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+sudo setenforce 1 && \
+sudo sed -i --follow-symlinks 's/^SELINUX=.*/SELINUX=enforcing/g' /etc/sysconfig/selinux
+
+sudo sed -i '$ d' /etc/resolv.conf
+sudo bash -c 'echo -e "nameserver 8.8.8.8\n" >> /etc/resolv.conf'
+
+sudo yum install -y yum-utils
+
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum makecache fast
+
+sudo yum install -y http://mirror.centos.org/centos/7/extras/x86_64/Packages/container-selinux-2.74-1.el7.noarch.rpm
+sudo yum install -y http://mirror.centos.org/centos/7/extras/x86_64/Packages/pigz-2.3.3-1.el7.centos.x86_64.rpm
+
+sudo yum install -y docker-ce-19.03.0 docker-ce-cli-19.03.0 containerd.io
+sudo systemctl enable docker
+sudo systemctl start docker
+
+sudo yum install -y wget
+sudo yum install -y git
+sudo yum install -y unzip
+sudo yum install -y curl
+sudo yum install -y xz
+sudo yum install -y ipset
+sudo yum install -y bind-utils
+sudo yum install -y ntp
+sudo systemctl enable ntpd
+sudo systemctl start ntpd
+sudo getent group nogroup || sudo groupadd nogroup
+sudo getent group docker || sudo groupadd docker
+sudo touch /opt/dcos-prereqs.installed
