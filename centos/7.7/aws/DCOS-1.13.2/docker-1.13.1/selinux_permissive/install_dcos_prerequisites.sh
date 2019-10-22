66fd098e5f5b036ba9d220697f6264360750e783
Arsen Hayrapetyan <ahairape@gmail.com>
2019-10-23 00:45:39 +0200

    Remove unnused docker repos

--- centos/7.7/aws/DCOS-1.13.2/docker-1.13.1/selinux_permissive/install_dcos_prerequisites.sh
+++ centos/7.7/aws/DCOS-1.13.2/docker-1.13.1/selinux_permissive/install_dcos_prerequisites.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 sudo setenforce 0 && \
 sudo sed -i --follow-symlinks 's/^SELINUX=.*/SELINUX=permissive/g' /etc/sysconfig/selinux
-sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
-[dockerrepo]
-name=Docker Repository
-baseurl=https://yum.dockerproject.org/repo/main/centos/7
-enabled=1
-gpgcheck=1
-gpgkey=https://yum.dockerproject.org/gpg
-EOF
+#sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
+#[dockerrepo]
+#name=Docker Repository
+#baseurl=https://yum.dockerproject.org/repo/main/centos/7
+#enabled=1
+#gpgcheck=1
+#gpgkey=https://yum.dockerproject.org/gpg
+#EOF
 
-sudo mkdir -p /etc/systemd/system/docker.service.d
-sudo tee /etc/systemd/system/docker.service.d/override.conf <<- EOF
-[Service]
-ExecStart=
-ExecStart=/usr/bin/dockerd -H fd://
-EOF
+#sudo mkdir -p /etc/systemd/system/docker.service.d
+#sudo tee /etc/systemd/system/docker.service.d/override.conf <<- EOF
+#[Service]
+#ExecStart=
+#ExecStart=/usr/bin/dockerd -H fd://
+#EOF
 
 sudo yum install -y yum-utils \
   device-mapper-persistent-data \
