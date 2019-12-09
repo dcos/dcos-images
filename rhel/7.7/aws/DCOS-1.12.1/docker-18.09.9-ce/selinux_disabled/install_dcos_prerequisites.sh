347e90b33224e16e10b49db3189f8f93267fc549
gauripowale <30297870+gauripowale@users.noreply.github.com>
2019-12-09 14:40:42 -0800

    Qualifying DC/OS 1.12.1 with RHEL 7.7 & Docker 18.09.9 EE & SELinux Disabled (#480)
    * Qualifying DC/OS 1.12.1 with RHEL 7.7 & Docker 18.09.9 EE & SELinux Disabled
    
    * Fixing docker EE repo
    
    * Fixing prereq script
    
    * Publish dcos_images.yaml for rhel/7.7/aws/DCOS-1.12.1/docker-18.09.9-ce/selinux_disabled

---
+++ rhel/7.7/aws/DCOS-1.12.1/docker-18.09.9-ce/selinux_disabled/install_dcos_prerequisites.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+sudo setenforce 0 && \
+sudo sed -i --follow-symlinks 's/^SELINUX=.*/SELINUX=disabled/g' /etc/sysconfig/selinux
+
+sudo sed -i '$ d' /etc/resolv.conf
+sudo bash -c 'echo -e "nameserver 8.8.8.8\n" >> /etc/resolv.conf'
+
+export DOCKERURL="https://storebits.docker.com/ee/m/sub-b02bc8f1-2d50-45f7-a1bd-2d020928b6e5"
+
+sudo -E sh -c 'echo "$DOCKERURL/rhel" > /etc/yum/vars/dockerurl'
+sudo sh -c 'echo "7" > /etc/yum/vars/dockerosversion'
+
+sudo yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+
+sudo yum-config-manager --enable rhel-7-server-extras-rpms
+sudo yum-config-manager --enable rhui-REGION-rhel-server-extras
+
+sudo -E yum-config-manager \
+    --add-repo \
+    "$DOCKERURL/rhel/docker-ee.repo"
+
+sudo yum -y install docker-ee-18.09.9 docker-ee-cli-18.09.9 containerd.io
+sudo systemctl enable docker
+sudo systemctl start docker
+
+sudo yum install -y wget
+sudo yum install -y git
+sudo yum install -y unzip
+sudo yum install -y curl
+sudo yum install -y xz
+sudo yum install -y ipset
+sudo yum install -y bind-utils
+sudo yum install -y ntp
+sudo systemctl enable ntpd
+sudo systemctl start ntpd
+sudo getent group nogroup || sudo groupadd nogroup
+sudo getent group docker || sudo groupadd docker
+sudo touch /opt/dcos-prereqs.installed
