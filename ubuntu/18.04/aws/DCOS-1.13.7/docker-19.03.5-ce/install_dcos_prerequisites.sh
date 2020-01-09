290f967e8ccb7b90ffc8b11e605ebf19ad2850ea
Arsen Hayrapetyan <ahairape@gmail.com>
2020-01-09 14:58:45 +0100

    Add symlinks for DC/OS installer

--- ubuntu/18.04/aws/DCOS-1.13.7/docker-19.03.5-ce/install_dcos_prerequisites.sh
+++ ubuntu/18.04/aws/DCOS-1.13.7/docker-19.03.5-ce/install_dcos_prerequisites.sh
@@ -7,7 +7,7 @@ overlay
 EOF
 
 sudo apt-get update
-sudo apt-get install \
+sudo apt-get -y install \
   apt-transport-https \
   ca-certificates \
   curl \
@@ -21,18 +21,25 @@ sudo add-apt-repository \
   $(lsb_release -cs) \
   stable"
 
-sudo apt-get install docker-ce="5:19.03.5~3-0~ubuntu-bionic" docker-ce-cli="5:19.03.5~3-0~ubuntu-bionic" containerd.io
+sudo apt-get -y install docker-ce="5:19.03.5~3-0~ubuntu-bionic" docker-ce-cli="5:19.03.5~3-0~ubuntu-bionic" containerd.io
 
 sudo systemctl start docker
 sudo systemctl enable docker
 
-sudo apt-get install ntp
+sudo apt-get -y install ntp
 sudo systemctl enable ntp
 sudo systemctl start ntp
-sudo apt-get install wget
-sudo apt-get install unzip
-sudo apt-get install git
-sudo apt-get install ipset
+sudo apt-get -y install wget
+sudo apt-get -y install unzip
+sudo apt-get -y install git
+sudo apt-get -y install ipset
+
+sudo ln -s /bin/rm /usr/bin/rm
+sudo ln -s /bin/tar /usr/bin/tar
+sudo ln -s /bin/ln /usr/bin/ln
+sudo ln -s /bin/mkdir /usr/bin/mkdir
+sudo ln -s /usr/sbin/useradd /usr/bin/useradd
+sudo ln -s /usr/sbin/groupadd /usr/bin/groupadd
 
 sudo systemctl disable systemd-resolved.service
 sudo systemctl stop systemd-resolved.service
