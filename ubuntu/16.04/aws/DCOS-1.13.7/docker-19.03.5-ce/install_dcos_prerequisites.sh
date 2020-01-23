f95fe8c67f44ff31477a012aec1afa8598adac8b
Arsen Hayrapetyan <ahairape@gmail.com>
2020-01-23 11:00:27 +0100

    Fix packer

--- ubuntu/16.04/aws/DCOS-1.13.7/docker-19.03.5-ce/install_dcos_prerequisites.sh
+++ ubuntu/16.04/aws/DCOS-1.13.7/docker-19.03.5-ce/install_dcos_prerequisites.sh
@@ -3,22 +3,23 @@ sudo tee /etc/modules-load.d/overlay.conf <<-'EOF'
 overlay
 EOF
 
+sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update
-sudo apt-get -y install \
-  apt-transport-https \
-  ca-certificates \
-  curl \
-  gnupg-agent \
-  software-properties-common
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository \
-  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) \
-  stable"
-
+      "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) \
+      stable"
 sudo apt-get update
+
 sudo apt-get -y install docker-ce="5:19.03.5~3-0~ubuntu-xenial" docker-ce-cli="5:19.03.5~3-0~ubuntu-xenial" containerd.io
 
 sudo systemctl start docker
