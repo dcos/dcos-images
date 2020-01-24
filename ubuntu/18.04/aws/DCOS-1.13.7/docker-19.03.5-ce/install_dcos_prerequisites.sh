14b603655ee42c914379995bcb3920ebba71f575
Arsen Hayrapetyan <ahairape@gmail.com>
2020-01-24 14:46:40 +0100

    Fix prerequisite setup

--- ubuntu/18.04/aws/DCOS-1.13.7/docker-19.03.5-ce/install_dcos_prerequisites.sh
+++ ubuntu/18.04/aws/DCOS-1.13.7/docker-19.03.5-ce/install_dcos_prerequisites.sh
@@ -1,27 +1,26 @@
 #!/usr/bin/env bash
-sudo sed -i '$ d' /etc/resolv.conf
-sudo bash -c 'echo -e "nameserver 8.8.8.8\n" >> /etc/resolv.conf'
-
 sudo tee /etc/modules-load.d/overlay.conf <<-'EOF'
 overlay
 EOF
 
+sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update
-sudo apt-get -y install \
-  apt-transport-https \
-  ca-certificates \
-  curl \
-  gnupg-agent \
-  software-properties-common
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository \
-  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) \
-  stable"
+      "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) \
+      stable"
+sudo apt-get update
 
-sudo apt-get -y install docker-ce="5:19.03.5~3-0~ubuntu-bionic" docker-ce-cli="5:19.03.5~3-0~ubuntu-bionic" containerd.io
+sudo apt-get -y install docker-ce="5:19.03.5~3-0~ubuntu-xenial" docker-ce-cli="5:19.03.5~3-0~ubuntu-xenial" containerd.io
 
 sudo systemctl start docker
 sudo systemctl enable docker
@@ -40,6 +39,7 @@ sudo ln -s /bin/ln /usr/bin/ln
 sudo ln -s /bin/mkdir /usr/bin/mkdir
 sudo ln -s /usr/sbin/useradd /usr/bin/useradd
 sudo ln -s /usr/sbin/groupadd /usr/bin/groupadd
+sudo ln -s  /bin/systemctl /usr/bin/systemctl
 
 #sudo systemctl disable systemd-resolved.service
 #sudo systemctl stop systemd-resolved.service
