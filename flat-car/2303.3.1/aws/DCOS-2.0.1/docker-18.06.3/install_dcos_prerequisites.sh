5ecfc635c0bc89c8784ad5a37c43ee2e58b82366
Arsen Hayrapetyan <ahairape@gmail.com>
2020-02-19 14:33:29 +0100

    Add DCOS-2.0.1

---
+++ flat-car/2303.3.1/aws/DCOS-2.0.1/docker-18.06.3/install_dcos_prerequisites.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+sudo systemctl disable locksmithd
+sudo systemctl stop locksmithd
+sudo systemctl mask locksmithd
+
+sudo systemctl disable update-engine # Disabling automatic updates.
+sudo systemctl stop update-engine
+sudo systemctl mask update-engine
+
+# sudo mkdir -p /etc/systemd/system/docker.service.d
+# sudo tee /etc/systemd/system/docker.service.d/override.conf <<- EOF
+# [Service]
+# ExecStart=
+# ExecStart=/usr/bin/dockerd -H fd://
+# EOF
+#
+# sudo systemctl restart docker # Restarting docker to ensure its ready. Seems like its not during first usage
