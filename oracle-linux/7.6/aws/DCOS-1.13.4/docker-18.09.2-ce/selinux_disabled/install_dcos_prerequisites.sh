e8ad76197ce1a610037b86b0a1c500071eaa6613
Arsen Hayrapetyan <ahayrapetyan@mesosphere.com>
2020-04-17 20:24:57 +0200

    Qualifying DC/OS 1.13.4 against OL 7.6 and Docker CE 19 (#341)
    * Initial commit
    
    * Add user oracle to the terraform template
    
    * Change packer.json configuration
    
    * Change ssh user
    
    * Specify packer.json ssh_user variable
    
    * Enable debugging
    
    * Enable packer debugging mode
    
    * Add packer debug
    
    * Fix typo
    
    * Use different OEL ami
    
    * Use official AMI
    
    * Change ssh_user
    
    * Change ssh user in packer.json
    
    * Remove os and ssh_user settings from cluster_profile.tfvars file
    
    * Add rhel ssh settings
    
    * Remove OS setting from cluster_profile.tfvars
    
    * Try innstallinng DC/OS 1.13.5
    
    * Try DC/OS 1.13.6 with XFS fix
    
    * Enable TF debug
    
    * Make configure_dcos_system.sh executable in packer config
    
    * Disable TF debug
    
    * Correct ami description in packer.json
    
    * Use dcos prereq installation script for RHEL
    
    * Re-trigger build
    
    * Disable extra configuration
    
    * Fix JSON syntax
    
    * Increase volume setup service timeout
    
    * Add dcos_resolver ip
    
    * Change dcos_resolvers type to list
    
    * Specify dcos_no_proxy
    
    * Fix syntax
    
    * Use Google DNS for host name resolution
    
    * Change DC/OS installer
    
    * Use deffault network settings
    
    * Use m5.xlarge for bootstrap
    
    * Add XFS fix
    
    * Add dns resolver
    
    * Using terraform settings from RHEL 7.6 qualifiation
    
    * Remove dcos_images.yaml and packer history file
    
    * Change packer instance type
    
    * Change dc/os setup steps
    
    * Fix packer.json syntax
    
    * Change instannce type
    
    * Change instance type again
    
    * Change DC/OS installer
    
    * Making iptable rules changes presistent
    
    * Run single failing tests
    
    * Publish dcos_images.yaml for oracle-linux/7.6/aws/DCOS-1.13.4/docker-18.09.2-ce/selinux_disabled
    
    Co-authored-by: mesosphere_jenkins <mesosphere_jenkins>

---
+++ oracle-linux/7.6/aws/DCOS-1.13.4/docker-18.09.2-ce/selinux_disabled/install_dcos_prerequisites.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+sudo setenforce 0 && \
+sudo sed -i --follow-symlinks 's/^SELINUX=.*/SELINUX=disabled/g' /etc/sysconfig/selinux
+
+#sudo sed -i '$ d' /etc/resolv.conf
+#sudo bash -c 'echo -e "nameserver 8.8.8.8\n" >> /etc/resolv.conf'
+
+sudo yum install -y yum-utils
+
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum makecache fast
+
+sudo yum install -y http://mirror.centos.org/centos/7/extras/x86_64/Packages/container-selinux-2.107-1.el7_6.noarch.rpm
+sudo yum install -y http://mirror.centos.org/centos/7/extras/x86_64/Packages/pigz-2.3.3-1.el7.centos.x86_64.rpm
+
+sudo yum install -y docker-ce-18.09.9 docker-ce-cli-18.09.9 containerd.io
+sudo systemctl enable docker
+sudo systemctl start docker
+
+sudo yum install -y wget
+sudo yum install -y git
+sudo yum install -y unzip
+sudo yum install -y curl
+sudo yum install -y xz
+sudo yum install -y ipset
+sudo yum install -y bind-utils
+sudo yum install -y ntp
+sudo systemctl enable ntpd
+sudo systemctl start ntpd
+sudo getent group docker || sudo groupadd docker
+sudo iptables -D INPUT -j REJECT --reject-with icmp-host-prohibited
+sudo iptables -D FORWARD -j REJECT --reject-with icmp-host-prohibited
+sudo service iptables save
+sudo touch /opt/dcos-prereqs.installed
