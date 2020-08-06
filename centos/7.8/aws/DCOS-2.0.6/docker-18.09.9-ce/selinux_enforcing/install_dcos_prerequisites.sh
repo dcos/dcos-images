3cf1e11e9f9859009c6b6b537a8b419b6ad2f89b
Dominik Dary <ddary@d2iq.com>
2020-08-06 11:00:02 +0200

    Running tests in sequence to avoid git issues.

--- centos/7.8/aws/DCOS-2.0.6/docker-18.09.9-ce/selinux_enforcing/install_dcos_prerequisites.sh
+++ centos/7.8/aws/DCOS-2.0.6/docker-18.09.9-ce/selinux_enforcing/install_dcos_prerequisites.sh
@@ -22,14 +22,12 @@ name=CentOS Extras
 baseurl=http://mirror.web-ster.com/centos/7.8.2003/extras/$basearch/
 gpgcheck=0
 enabled=1
-[local-docker-ce]
-name=Docker Repository
-baseurl=https://download.docker.com/linux/centos/7/$basearch/stable
-enabled=1
-gpgcheck=0
-#gpgkey=https://yum.dockerproject.org/gpg
 EOF
 
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+    
 sudo yum clean all
 sudo yum repolist -v
 
